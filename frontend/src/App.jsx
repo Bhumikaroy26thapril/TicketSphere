42e788a587fd1c75cc2df456598cb5abc1984e47
Bhumikaroy26thapril <98340739+Bhumikaroy26thapril@users.noreply.github.com>
2024-07-30 04:05:05 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import Navbar from "./components/Navbar";
+import Home from "./components/Auth/Home";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import TicketForm from "./components/Tickets/TicketForm";
 import TicketList from "./components/Tickets/TicketList";
 import TicketDetails from "./components/Tickets/TicketDetails";
-import './index.css';
+import "./index.css";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <Navbar />
         <div className="container mx-auto p-4">
           <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/tickets" element={<TicketList />} />
